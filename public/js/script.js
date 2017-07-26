6f56f57636f296e845ea105edb1e768868370e7c
leiquan <leiquan@live.com>
2017-07-27 01:56:11 +0800

    更新

--- public/js/script.js
+++ public/js/script.js
@@ -135,3 +135,23 @@
     $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
+
+window.onload = function () {
+  var btn = document.getElementById('music-btn');
+  var music = document.getElementById('love-music');
+
+  var isPlay = 0;
+
+  btn.onclick = function () {
+
+    if (isPlay == 0) {
+      isPlay = 1;
+      music.play();
+    } else {
+      isPlay = 0;
+      music.pause();
+    }
+
+  };
+
+};
